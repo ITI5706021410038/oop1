703ab190b708903be82024de0066af3f3c8a4c7d
ITI5706021410038 <5706021410038@fitm.kmutnb.ac.th>
2016-02-18 21:43:00 +0700

    Update homework1.java

--- src/homework1.java
+++ src/homework1.java
@@ -1,4 +1,17 @@
 package h1;
+import java.io.*;
+import javax.swing.JOptionPane;
+import java.util.*;
+public class homework1
+{
+	int Sec,Min,Hour,Day;
+	public void getData()
+	{
+		Sec = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter Sec","Input",JOptionPane.QUESTION_MESSAGE));
+	}
+	
+	
+}
 
 public class homework1 {
 
