bf896ff1a73481f6a78f3c1edfd649439d853105
ITI5706021410038 <5706021410038@fitm.kmutnb.ac.th>
2016-02-19 22:51:52 +0700

    Update homework1.java

--- src/homework1.java
+++ src/homework1.java
@@ -9,6 +9,22 @@ public class homework1
 	{
 		Sec = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter Sec","Input",JOptionPane.QUESTION_MESSAGE));
 	}
+	public void Calc()
+	{
+		Min = Sec / 60;
+		Sec = Sec % 60;
+		
+		Hour = Min / 60;
+		Min = Min % 60;
+		
+		Day = Hour/24;
+		Hour = Hour % 24;
+	}
+	public void Print()
+	{
+		
+		JOptionPane.showMessageDialog(null,"Time \n Day:Hour:Min:Sec\n "+Day+":"+Hour+":"+Min+":"+Sec,"Message",JOptionPane.INFORMATION_MESSAGE);
+	}
 	
 	
 }
