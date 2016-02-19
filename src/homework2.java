f9232aaff9ddc73d0054e20a3a99e75a9d0d2529
ITI5706021410038 <5706021410038@fitm.kmutnb.ac.th>
2016-02-19 22:57:21 +0700

    Update homework2.java

--- src/homework2.java
+++ src/homework2.java
@@ -1,10 +1,14 @@
 package h1;
-
-public class homework2 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+import java.io.*;
+import javax.swing.JOptionPane;
+import java.util.*;
+public class homework2
+{
+	int A,E,I,O,U;
+	
+	public String getData()
+	{
+		String name = JOptionPane.showInputDialog(null,"Enter name ","InputData",JOptionPane.QUESTION_MESSAGE);
+		name = name.toUpperCase();
+		return name;
 	}
-
-}
