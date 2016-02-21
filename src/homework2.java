0ff93c015104a10b0eac061c9e959a8030778b7d
ITI5706021410038 <5706021410038@fitm.kmutnb.ac.th>
2016-02-21 13:33:40 +0700

    Update homework2.java

--- src/homework2.java
+++ src/homework2.java
@@ -12,3 +12,30 @@ public class homework2
 		name = name.toUpperCase();
 		return name;
 	}
+	public void Calc(String name)
+	{
+		A=E=I=O=U=0;
+		for(int i=0;i<name.length();i++)
+		{
+			if(name.charAt(i) == 'A')
+			{
+				A=A+1;
+			}
+			else if(name.charAt(i) == 'E')
+			{
+				E=E+1;
+			}
+			else if(name.charAt(i) == 'I')
+			{
+				I=I+1;
+			}
+			else if(name.charAt(i) == 'O')
+			{
+				O=O+1;
+			}
+			else if(name.charAt(i) == 'U')
+			{
+				U=U+1;
+			}
+		}
+	}
