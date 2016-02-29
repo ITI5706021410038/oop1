60d428d886c0a1237af36a088c36765ff43adccb
ITI5706021410038 <5706021410038@fitm.kmutnb.ac.th>
2016-02-29 22:03:21 +0700

    Update hw1.java

--- src/hw1.java
+++ src/hw1.java
@@ -17,8 +17,36 @@ package h2;
 			Sum = new String[40][2];
 			
 			n1 = "Number\tMidterm\tFinal\tHomework\tAssignment\tPoint\n";
+			for(int i=0;i<Sec3.length;i++)
+			{
+				n1 = n1+(i+1)+"\t";
+				for(int j=0;j<Sec3[0].length;j++)
+				{
+					if(j <2)
+					{
+						Sec3[i][j] = rand.nextInt(31);
+					}
+					else if(j == 2)
+					{
+						Sec3[i][j] = rand.nextInt(16);
+					}
+					else if(j == 3)
+					{
+						Sec3[i][j] = rand.nextInt(21);
+					}
+					else
+					{
+						Sec3[i][j] = rand.nextInt(6);
+					}
+					a = a + Sec3[i][j];
+					n1 = n1 + Sec3[i][j]+"\t";
+				}
+				n1=n1+"\n";
+				Sum[i][0] = Integer.toString(a);
+				a = 0;
+			}
+			return n1;
+		}
+		
 		
 
-	}
-
-}
