c59c3e3f3ab66fa92dafc09b53d3dc585ffeac50
ITI5706021410038 <5706021410038@fitm.kmutnb.ac.th>
2016-02-29 22:02:53 +0700

    Update hw1.java

--- src/hw1.java
+++ src/hw1.java
@@ -10,6 +10,13 @@ package h2;
 		String[][] Sum;
 		int a=0;
 		Random rand = new Random();
+		public String OOPSec3()
+		{
+			String n1;
+			Sec3 = new int[40][5];
+			Sum = new String[40][2];
+			
+			n1 = "Number\tMidterm\tFinal\tHomework\tAssignment\tPoint\n";
 		
 
 	}
